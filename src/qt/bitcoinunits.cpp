37257b49064da541753a7aa518698f37dbb9d46b
IlfirinCano <harnainu@mail.ru>
2017-04-17 18:52:23 +0500

    names

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("SVC");
+    case mBTC: return QString("mSVC");
+    case uBTC: return QString::fromUtf8("μSVC");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Shavercoins");
+    case mBTC: return QString("Milli-Shavercoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Shavercoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
