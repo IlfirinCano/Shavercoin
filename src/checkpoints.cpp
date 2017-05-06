32d4dab93cf46db5bd914c1db21d4d4e38bd6dee
IlfirinCano <harnainu@mail.ru>
2017-05-06 14:36:48 +0500

    cp
    cp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -35,7 +35,7 @@ namespace Checkpoints
     // + Contains no strange transactions
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-        (0, uint256("0xee566befd24f6bf149cf47c6a50e016faa0c968175808b94c0579657dd9cc0c4")),
+        (0, uint256("0xee566befd24f6bf149cf47c6a50e016faa0c968175808b94c0579657dd9cc0c4"))
 		(4, uint256("0xc93c23b02d96bf03677da212432b61716509226e382282b9a7010308bc3a0283"))
         ;
     static const CCheckpointData data = {
